0dcb11f6260d0c848ed0adad2448dbb29d18bb97
XiaoMuCOOL <895355044@qq.com>
2016-01-24 18:59:31 +0800

    update webpack environment

--- Webpack/js/main.js
+++ Webpack/js/main.js
@@ -1,6 +1,5 @@
-// var fn = ()=>`Hello ${name}, how are you ${time}?`;
-var name = "XiaoMu", time = "today";
-// var str = fn();
-console.log(name);
-// require('../css/style.css');
-require("style/url!file!../css/style.css");
+var fn = ()=>`Hello ${name}, how are you ${time}?`;
+var name = "XiaoMuCOOL", time = "today";
+var str = fn();
+console.log(str);
+require("css/style.scss");
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -16,26 +16,28 @@ module.exports = {
     //加载器配置
     //"-loader"其实是可以省略不写的，多个loader之间用“!”连接起来
     loaders: [{
-      test: /\.css$/, loader: 'style!css'
-    },/*{
-      test: /\.js$/, loader: 'babel?harmony'
-    },*/{
-      test: /\.scss$/, loader: 'style!css!sass'
+     test: /\.css$/, loader: 'style!css'
     },{
-      test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'
+     test: /\.js$/, loader: 'babel',query: {presets: ['es2015']}
+    },{
+     test: /\.scss$/, loader: 'style!css!sass'
+    },{
+     test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'
     }]
   },
   //其它解决方案配置
   resolve: {
     //查找module的话从这里开始查找:绝对路径
-    // root: 'C:/Workspace/XiaoMuCOOL/newskills/Webpack', 
+    root: 'C:/Workspace/XiaoMuCOOL/newskills/Webpack', 
     //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
     // extensions: ['', '.js', '.json', '.scss'],
     //模块别名定义，方便后续直接引用别名，无须多写长长的地址
-    // alias: {
-    //   AppStore : 'js/stores/AppStores.js',//后续直接 require('AppStore') 即可
-    //   ActionType : 'js/actions/ActionType.js',
-    //   AppAction : 'js/actions/AppAction.js'
-    // }
+    alias: {
+      // AppStore : 'js/stores/AppStores.js',//后续直接 require('AppStore') 即可
+      // ActionType : 'js/actions/ActionType.js',
+      css : path.join(__dirname, 'css/'),
+      img : path.join(__dirname, 'img/'),
+      js : path.join(__dirname, 'js/'),
+    }
   }
 };
